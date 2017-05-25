476c36bdfdbdb94e482489c741be38981f461345
pupepon <x16075xx@aitech.ac.jp>
2017-05-25 14:01:19 +0900

    05/25

---
+++ irekawatteruDiary/irekawatteruDiary/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  irekawatteruDiary
+//
+//  Created by x16075xx on 2017/05/20.
+//  Copyright © 2017年 AIT. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
